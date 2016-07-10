4a1e94f7dec4f83d92f7924010276bcc8bd3295c
Francesco Giannelli <bassobassista@gmail.com>
2016-07-10 11:07:12 +0000

    Update toucher.sh

--- scripts/toucher.sh
+++ scripts/toucher.sh
@@ -2,12 +2,12 @@
 
 echo "touching list.txt"
 echo "end is `cat /host/list.txt | tail -n 1`" 
-i=0
-while read in;
-do
-  sleep 0.01
-  touch "$in"
-  sleep 0.01
-done < /host/list.txt
+touch `cat /host/list.txt`
+#while read in;
+#do
+#  sleep 0.01
+#  touch "$in"
+#  sleep 0.01
+#done < /host/list.txt
 echo "done touching list.txt"
 
