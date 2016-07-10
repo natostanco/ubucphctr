7983ed44e4dea08e0e1dc8aa23d41c326ec75a84
Francesco Giannelli <bassobassista@gmail.com>
2016-07-10 10:31:47 +0000

    Update toucher.sh

--- scripts/toucher.sh
+++ scripts/toucher.sh
@@ -5,13 +5,12 @@ echo "end is `cat /host/list.txt | tail -n 1`"
 i=0
 while read in;
 do
-  touch "$in" &>/dev/null &
-  #if [[ "$(( $i % 25 ))" == "0" ]] ; then
-  #  i=$(( $i + 1 ))
+  i=$(( $i + 1 ))
+  touch "$in"
+  if [[ "$(( $i % 25 ))" == "0" ]] ; then
     echo $in
     sleep 0.3
-  #  sleep 2
-  #fi
+  fi
 done < /host/list.txt
 echo "done touching list.txt"
 
