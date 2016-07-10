e74c8631d86b4cc3682d8ec605021154da3477a4
Francesco Giannelli <bassobassista@gmail.com>
2016-07-10 09:41:17 +0000

    Update toucher.sh

--- scripts/toucher.sh
+++ scripts/toucher.sh
@@ -6,11 +6,11 @@ i=0
 while read in;
 do
   touch "$in"
-  if [[ "$(( $i % 25 ))" == "0" ]] ; then
-    i=$(( $i + 1 ))
+  #if [[ "$(( $i % 25 ))" == "0" ]] ; then
+  #  i=$(( $i + 1 ))
     echo $in
-    sleep 2
-  fi
+  #  sleep 2
+  #fi
 done < /host/list.txt
 echo "done touching list.txt"
 
