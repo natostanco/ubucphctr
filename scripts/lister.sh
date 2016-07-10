05410b0246ec777757a16dd208b00b727774ef4a
Francesco Giannelli <bassobassista@gmail.com>
2016-07-10 17:31:36 +0000

    Update lister.sh

--- scripts/lister.sh
+++ scripts/lister.sh
@@ -53,14 +53,14 @@ include=( "/usr/bin/ceph*" \
 
 for n in ${!include[*]}
 do
-find ${include[n]} | xargs -I {} ldd {} | awk '{print $3}' | grep '/' | sort -u | xargs -I {} echo {} >> /tmp/touch
+find ${include[n]} | xargs -I {} ldd {} 2>/dev/null | awk '{print $3}' | grep '/' | sort -u | xargs -I {} echo {} >> /tmp/touch
 done
 cat /tmp/touch | sort -u >> /host/manuallist.txt
 rm /tmp/touch
 
 while read in;
 do
-ldd $in| awk '{print $3}' | grep '/' | sort -u | xargs -I {} echo {} >> /tmp/touch
+ldd $in 2>/dev/null | awk '{print $3}' | grep '/' | sort -u | xargs -I {} echo {} >> /tmp/touch
 done < /host/list.txt
 cat /tmp/touch | sort -u >> /host/lddlist.txt
 rm /tmp/touch
